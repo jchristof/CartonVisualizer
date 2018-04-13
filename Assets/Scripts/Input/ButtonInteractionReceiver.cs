bb63ed2c3973616f38729d1daac012e9675837b7
Jon Martin <jchristofmartin@gmail.com>
2018-04-13 17:17:00 -0500

    adding an app menu prefab experiment

---
+++ Assets/Scripts/Input/ButtonInteractionReceiver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.Receivers;
+using UnityEngine;
+
+public class ButtonInteractionReceiver : InteractionReceiver {
+    protected override void InputDown(GameObject obj, InputEventData eventData) {
+        base.InputDown(obj, eventData);
+    }
+}
