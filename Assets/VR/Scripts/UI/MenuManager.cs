9ad0f2f8d86fd01d1783686167946241c1cd25d8
Jon Martin <jchristofmartin@gmail.com>
2018-04-22 07:51:42 -0500

    adding speech derived from ssml to improve spoken phrase pace

--- Assets/VR/Scripts/UI/MenuManager.cs
+++ Assets/VR/Scripts/UI/MenuManager.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
 using HoloToolkit.Unity;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
@@ -22,6 +25,7 @@ namespace Assets.VR.Scripts.UI {
 
         private KeywordRecognizer keywordRecognizer;
         private TextToSpeech textToSpeech;
+        private Stack<DialogType> dialogStack = new Stack<DialogType>();
 
         void Start () {
             currentUI = Instantiate(welcomeDialog, gameObject.transform); 
@@ -33,6 +37,8 @@ namespace Assets.VR.Scripts.UI {
 
             textToSpeech = gameObject.GetComponentInChildren<TextToSpeech>();
             audioSource = gameObject.GetComponentInChildren<AudioSource>();
+
+            SpeakTheMenuText(currentUI);
         }
 
         private void KeywordRecognizerOnOnPhraseRecognized(PhraseRecognizedEventArgs args) {
@@ -53,6 +59,8 @@ namespace Assets.VR.Scripts.UI {
             audioSource.PlayOneShot(buttonClick);
     
             var dialog = currentUI.GetComponentInChildren<IDialog>();
+            dialogStack.Push(dialog.DialogType);
+
             Destroy(currentUI);
 
             if (dialog.DialogType == DialogType.Welcome) {
@@ -82,7 +90,23 @@ namespace Assets.VR.Scripts.UI {
                 currentUI.GetComponentInChildren<IDialog>().DialogResult = Result;
             }
 
-            textToSpeech.StartSpeaking(TitleText(currentUI) + MessageText(currentUI));
+            SpeakTheMenuText(currentUI);
+        }
+
+        private void SpeakTheMenuText(GameObject currentUI) {
+            var titleText = TitleText(currentUI).Replace("\n", "").Replace("\r", "");
+            var message = MessageText(currentUI).Replace("\n", "").Replace("\r", "");
+
+            string ssml = String.Format(@"<speak>{0} <break time=""1s""/> {1}</speak>", titleText, message);
+            string ssmlx = String.Format(@"<?xml version='1.0' encoding='utf-8'?>
+                <speak
+                  version=""1.0""
+                  xmlns=""http://www.w3.org/2001/10/synthesis""
+                  xml:lang=""en-US"">
+                  {0} <break time=""1s""/> {1}
+                </speak>", titleText, message);
+
+            textToSpeech.SpeakSsml(ssmlx);
         }
 
         private static string TitleText(GameObject currentUI) {
